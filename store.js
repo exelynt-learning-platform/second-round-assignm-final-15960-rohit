3d690fd492ea45d721d578c3a58301a44290d809
rohitkorade9199 <99752588+rohitkorade9199@users.noreply.github.com>
2026-03-31 15:54:56 +0530

    Delete store.js

--- store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import chatReducer from "../features/chat/chatSlice";
-
-export const store = configureStore({
-  reducer: {
-    chat: chatReducer,
-  },
-});
